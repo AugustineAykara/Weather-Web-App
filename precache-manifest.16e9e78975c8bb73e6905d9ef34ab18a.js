caeeb4197c7f24d88a6cb730f9b22c2b306e68bb
AugustineAykara <augustineaykara@gmail.com>
2019-07-08 13:25:18 +0530

    Updates

---
+++ precache-manifest.16e9e78975c8bb73e6905d9ef34ab18a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a500904eed14adc75fa56badf3c3d31d",
+    "url": "/Weather-Web-App/index.html"
+  },
+  {
+    "revision": "9bc6c79da7ef834a16eb",
+    "url": "/Weather-Web-App/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "4bf7840afe02e29aafba",
+    "url": "/Weather-Web-App/static/css/main.245ea75d.chunk.css"
+  },
+  {
+    "revision": "9bc6c79da7ef834a16eb",
+    "url": "/Weather-Web-App/static/js/2.e597a5a4.chunk.js"
+  },
+  {
+    "revision": "4bf7840afe02e29aafba",
+    "url": "/Weather-Web-App/static/js/main.dc250a37.chunk.js"
+  },
+  {
+    "revision": "98deb1221abbdcbffe4d",
+    "url": "/Weather-Web-App/static/js/runtime~main.40d86416.js"
+  }
+]);
